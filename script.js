9b160fbb6ccd71abcd2ddbc692bad556ef074999
alimohamedkurijan <109865837+alimohamedkurijan@users.noreply.github.com>
2022-08-24 09:18:13 -0700

    done with bounes

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // أنشئ المصفوفة تحت هذا الكومنت
-
-
+let orders = [];
 function addOrder() {
   let name = document.getElementById("name").value;
   let id = document.getElementById("id").value;
@@ -8,16 +7,31 @@ function addOrder() {
   let order = document.getElementById("order").value;
   
   // أنشئ كائن newOrder
-  
+  let newOrder = {name, id, phone, order,}
   // أدرج الكائن إلى المصفوفة
-  
+  orders.push(newOrder)
   // طبق الخطوة 7 تحت هذا الكومنت
-  
+  load()
 }
 
 function load() {
+
+
   let container = document.getElementById("container");
-  
+  container.innerHTML = ``;
   // طبّق الخطوة 6 تحت هذا الكومنت
+  orders.forEach(element => {
+    container.innerHTML +=  `<div><h1>${element.name} - ${element.id}</h1><h3>${element.order}</h3></div>`;
+    
+
+});
+}
+
+function Removeitem(){
+
+  const element = document.getElementById("container");
+  element.remove();
+
+  
 
 }
